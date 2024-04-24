4f46b776849f3ab4a5f242029a9197204ec3a6fc
laurelbee <157080961+laurelbee@users.noreply.github.com>
2024-04-24 17:38:33 -0400

    update to template

--- foliumtemplate.py
+++ foliumtemplate.py
@@ -2,11 +2,29 @@
 import folium as f
 
 # initialize map
-map = f.Map(location = [40.054671, -75.221652], zoom_start = 13)
+#map = f.Map(location = [40.054671, -75.221652], zoom_start = 13)
 
-#add geoJSON data to map
+##############################################################################
+
+## stylize the map ##
+# change scale and zoom levels
+
+# change basemap tiles example
+f.Map(location=[40.054671, -75.221652], tiles="Cartodb dark_matter", zoom_start= 13)
+
+##############################################################################
+## add data to map ##
+# geoJSON file example
 trails_file = ("C:/Users/tus97759/Desktop/GIS_Programming_ClassNotes/FoliumPractice/Wissahickon_Yellow_Trail_Loop.js")
 f.GeoJson(trails_file).add_to(map)
+
+##############################################################################
+## add markers and popups ##
+# adding markers 
+
+
+# adding popups
+
 
 #save map as html file so we can view it
 map.save('C:/Users/tus97759/Desktop/GIS_Programming_ClassNotes/FoliumPractice/mappractice.html')
